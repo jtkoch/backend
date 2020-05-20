b9205e8cc5519b8106123aaf33786be7b1d51e44
Jensen Koch <jtkoch12@gmail.com>
2020-05-20 13:03:39 -0500

    basic project setup, all dependencies installed

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# backend
-REPO FOR BACKEND ONLY
+Expat Journal - Back End Code Base
+
+Getting Started
+  - This is a set of instructions for you to get a copy of the project up and running on your local machine for development and testing purposes. Below are some notes on how to deploy and install the project on your machine. 
+
+Installing
+  - first cd into the server folder and install dependencies with:
+  npm install
+
+  - then launch the api with:
+  npm run server
+  
+    
---
+++ knexfile.js
@@ -0,0 +1,25 @@
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: { filename: './data/auth.db3' },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+      tableName: 'dbmigrations',
+    },
+    seeds: { directory: './data/seeds' },
+  },
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/testdatabase.db3',
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
+}
